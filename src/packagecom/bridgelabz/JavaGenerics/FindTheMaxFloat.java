c81c4c55072156d59759e9153c08952b7006bf08
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-01-19 02:04:09 -0800

    Finding The Max Value Using Generic

--- src/packagecom/bridgelabz/JavaGenerics/FindTheMaxFloat.java
+++ src/packagecom/bridgelabz/JavaGenerics/FindTheMaxFloat.java
@@ -14,7 +14,7 @@ public class FindTheMaxFloat
     }
 
     public static void main(String[] args) {
-        FindTheMaxInteger s = new FindTheMaxInteger();
+        FindTheMaxFloat s = new FindTheMaxFloat();
         System.out.println("max Number for 1st test case="+s.compareTo(5.7f,3.2f,2.5f));//Test case 1 :given max number for 1st position.
         System.out.println("max Number for 2nd test case="+s.compareTo(2.8f,6.5f,3.9f));//Test case 2:given max number for 2nd position.
         System.out.println("max Number for 3rd test case="+s.compareTo(1.5f,2.5f,3.9f));//Test case 3:given max number for the 3rd position.
